c6f6862f8196b733362549a81bda3c49f1cfcd5d
Artur <aat2812@yandex.ru>
2022-02-10 20:31:35 +0300

    Аптека 2.0

--- artur.py
+++ artur.py
@@ -2,8 +2,8 @@ import sys
 from io import BytesIO
 from pprint import pprint
 import requests
-from PIL import Image
-from funcs import find_spn
+from PIL import Image, ImageDraw, ImageFont
+from funcs import find_apteka, find_spn, lonlat_distance
 
 toponym_to_find = " ".join(sys.argv[1:])
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
@@ -12,8 +12,9 @@ geocoder_params = {
     "geocode": toponym_to_find,
     "format": "json"}
 response = requests.get(geocoder_api_server, params=geocoder_params)
+
 if not response:
-    pass
+    raise AttributeError(response.content)
 
 json_response = response.json()
 toponym = json_response["response"]["GeoObjectCollection"][
@@ -22,13 +23,27 @@ toponym_coodrinates = toponym["Point"]["pos"]
 toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
 
 delta = f"{find_spn(toponym_to_find)}"
+
+apteka_coords, info = find_apteka(",".join([toponym_longitude, toponym_lattitude]))
+distance = round(lonlat_distance(map(float, apteka_coords), (float(toponym_longitude), float(toponym_lattitude))))
+info = f'{info}\ndistance - {distance} m'
+print('\u2013')
 map_params = {
     "ll": ",".join([toponym_longitude, toponym_lattitude]),
-    "spn": ",".join([delta, delta]),
     "l": "map",
-    "pt": f'{",".join([toponym_longitude, toponym_lattitude])},pmdos'
+    "pt": f'{",".join([toponym_longitude, toponym_lattitude])},pmdos1~{",".join(map(str, apteka_coords))},pmgns2',
 }
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 response = requests.get(map_api_server, params=map_params)
-Image.open(BytesIO(
-    response.content)).show()
+im = Image.open(BytesIO(response.content))
+draw_text = ImageDraw.Draw(im)
+im.load()
+draw_text.text((0, 100 - 60),
+               info.split('\n')[0], fill=(100, 100, 100))
+draw_text.text((0, 120 - 60),
+               info.split('\n')[1], fill=(100, 100, 100))
+draw_text.text((0, 140 - 60),
+               info.split('\n')[2].replace('\u2013', '-'), fill=(100, 100, 100))
+draw_text.text((0, 160 - 60),
+               info.split('\n')[3], fill=(100, 100, 100))
+im.show()
--- funcs.py
+++ funcs.py
@@ -1,18 +1,50 @@
+from pprint import pprint
+from math import radians, cos, sqrt
+
 import requests as req
 
 
-def find_spn(address):
+def lonlat_distance(a, b):
+    degree_to_meters_factor = 111 * 1000
+    a_lon, a_lat = a
+    b_lon, b_lat = b
+    radians_lattitude = radians((a_lat + b_lat) / 2.)
+    lat_lon_factor = cos(radians_lattitude)
+    dx = abs(a_lon - b_lon) * degree_to_meters_factor * lat_lon_factor
+    dy = abs(a_lat - b_lat) * degree_to_meters_factor
+    distance = sqrt(dx * dx + dy * dy)
+    return distance
+
+
+def quer(name, address=None):
     quer = "https://search-maps.yandex.ru/v1/"
     api_key = 'dda3ddba-c9ea-4ead-9010-f43fbc15c6e3'
     params = {
         "apikey": api_key,
-        "text": 'аптека',
-        "lang": "ru_RU",
+        "text": name,
+        "lang": "en_US",
         "type": "biz",
-        'results': '1',
-        'address': address,
+        'results': '1'
     }
-    response = req.get(quer, params=params)
+    if address:
+        params['ll'] = address
+    return req.get(quer, params=params)
+
+
+def find_apteka(address):
+    response = quer('Аптека', address=address)
+    if not response:
+        raise AttributeError(response.content)
+    res = response.json()
+    coords = res['features'][0]['geometry']['coordinates']
+    info = res['features'][0]['properties']['CompanyMetaData']
+    name = f"Pharmacy - {info['name']} \naddress: {info['address']}"
+    name = f"{name} \nwork time: {info['Hours']['text']}"
+    return coords, name
+
+
+def find_spn(toponym):
+    response = quer(toponym)
     if not response:
         raise AttributeError(response.content)
     res = response.json()
